d96c29db1b0f460428f4f3a0487adaa1401285de
yodsapon64 <yodsaponb64@nu.ac.th>
2023-08-19 15:01:34 +0700

    'Page82'

---
+++ server/src/app.js
@@ -0,0 +1,29 @@
+let express = require("express");
+let bodyParser = require("body-parser")
+const { sequelize } = require('./models')
+
+const config = require('./config/config')
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+require('./routes')(app)
+
+app.get("/status", function (req, res) {
+    res.send("Hello nodejs server belong to Poom");
+});
+
+app.get("/hello/:name", function (req, res) {
+    console.log("hello - " + req.params.name);
+    res.send("sey hello with" + req.params.name);
+});
+
+let port = process.env.PORT || config.port
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(port, function () {
+        console.log("server running on " + port);
+    })
+})
