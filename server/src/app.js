65ec7bc371e8ec6ff2d0fda48a499eec282d5ab2
Poomeeii55 <poomeeii55@gmail.com>
2024-09-15 20:42:41 +0700

    new commit

--- server/src/app.js
+++ server/src/app.js
@@ -1,29 +1,25 @@
-let express = require("express");
-let bodyParser = require("body-parser")
-const { sequelize } = require('./models')
+let express = require('express');
 
-const config = require('./config/config')
+let bodyParser = require('body-parser');
+const cors = require('cors');
+const { sequelize } = require('./models');
+const config = require('./config/config');
+
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
-require('./routes')(app)
 
-app.get("/status", function (req, res) {
-    res.send("Hello nodejs server belong to Poom");
-});
+require('./routes')(app);
 
-app.get("/hello/:name", function (req, res) {
-    console.log("hello - " + req.params.name);
-    res.send("sey hello with" + req.params.name);
-});
 
-let port = process.env.PORT || config.port
+let port = config.port;
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(port, function () {
-        console.log("server running on " + port);
+        console.log('Server running on ' + port)
     })
 })
